d8307edee4816f96c431ca07043aedac9118f71b
Özgür Can Altınok <48184394+OzgurAltinok@users.noreply.github.com>
2019-04-16 17:48:56 +0300

    Update RegexNotFoundException.cs

--- Lab07Temp/Lab07/RegexNotFoundException.cs
+++ Lab07Temp/Lab07/RegexNotFoundException.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +6,24 @@ using System.Threading.Tasks;
 
 namespace Lab07
 {
-    class RegexNotFoundException : Exception
+    public class RegexNotFoundException : Exception
     {
 
         //Hiç argüman almayan, Sadece mesajı alan , Mesaj ve iç exception'i alan 3 farklı consturoctor tanımlanacak.
+
+        public RegexNotFoundException()
+        {
+
+        }
+
+        public RegexNotFoundException(string message)
+        {
+
+        }
+
+        public RegexNotFoundException(string message, Exception e)
+        {
+
+        }
     }
 }
